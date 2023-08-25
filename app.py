227414a425ed8be97ab94629892bb7e1590d4795
marait123 <mohammedibrahimgaballah@gmail.com>
2023-08-25 16:52:09 +0300

    added initial code

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
 # initialize flask app
+
+from flask import Flask
+
+
+def create_app():
+    """Create and configure an instance of the Flask application."""
+    app = Flask(__name__)
+
+    # a simple page that says hello
+    @app.route('/')
+    def hello():
+        return 'Hello, World!'
+
+    return app
